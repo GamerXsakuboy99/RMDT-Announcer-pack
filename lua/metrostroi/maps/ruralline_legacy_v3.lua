3a815308203ff0f6a1fd46a403bb08d864a88676
RekrutKurbel <rekrutkurbel@mail.ru>
2025-01-18 14:15:03 +0100

    ticker messages

--- lua/metrostroi/maps/ruralline_legacy_v3.lua
+++ lua/metrostroi/maps/ruralline_legacy_v3.lua
@@ -1025,5 +1025,13 @@ Metrostroi.AddLastStationTex("720", 906, "metrostroi_skins/81-717_schemes/906-rl
 Metrostroi.AddPassSchemeTex("717_new", "Ruralline_1995", {"metrostroi_skins/81-717_schemes/717-ruralline_v3-map",})
 Metrostroi.AddPassSchemeTex("720", "Ruralline_1995", {"metrostroi_skins/81-720_schemes/720-linemap-ruralline-v3-left", "metrostroi_skins/81-720_schemes/720-linemap-ruralline-v3-right",})
 Metrostroi.AddPassSchemeTex("722", "Ruralline_1995", {"metrostroi_skins/81-722_schemes/ruralline_1995r", "metrostroi_skins/81-722_schemes/ruralline-1995",})
-Metrostroi.TickerAdverts = {"....No..Connection...to..Ticker..server..Error..505.....", "......................."}
+Metrostroi.TickerAdverts = {
+	"DEAR PASSENGERS, WHEN LEAVING THE TRAIN, PLEASE DO NOT FORGET TO TAKE YOUR BELONGINGS WITH YOU.",
+	"FROM THE 6TH OF JULY 1995 ONWARDS, THE USAGE OF THE LAKEVIEW SUBWAY WILL BE FREE OF CHARGE.", 
+	"THE REALIGNMENT OF THE MARKET STREET LINE BETWEEN WHITE FOREST AND ROCKLAKE IS ESTIMATED TO OPEN BY JUNE 1997. THE STATIONS WHITE FOREST AND URBAN PARK WILL BE CLOSED AND REPLACED.",
+	"DEAR PASSENGERS, FOR YOUR OWN SAFETY, PLEASE HOLD ON TO THE HANDRAILS.",
+	"FROM THE 6TH OF JULY 1995 ONWARDS, THE UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE.......................................",
+	".",
+	"ROCKPORT METROPOLITAN UNIVERSE CREATED BY GAMERZXSAKUZBOY99. ROCKPORTMETROPOLITAN.EU"
+}
 print("RMDT Announcer loader: RPM Announcer has now loadded fully.")
